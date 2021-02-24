6f18a8c7e52c04cc1dc1294241c939053a335027
william-zhang-fullstack <william.zhang.fullstack@gmail.com>
2021-02-24 13:02:55 -0500

    add single static page using render_template

---
+++ webapps/02-templates/__main__.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template  # pre-code for Flask webapps
+
+
+app = Flask(__name__)  # the webapp itself
+
+
+@app.route('/')  # how Flask specifies webpages
+def index():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':  # this runs the app
+    app.run()
